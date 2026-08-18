03a9d5608b9de3aa77f764a161fb821a6ba7ba5b
R8152 <raulteles05@gmail.com>
2026-08-18 20:14:55 -0300

    fix: conserta useEffect

--- taskManager/src/App.jsx
+++ taskManager/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import TaskForm from './components/TaskForm'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const taskList = [
+    {
+      title: "Limpar a caixa dos gatos",
+      descricao: "Limpar com uma pá e um saco de plástico",
+      priori: "Média",
+      situacao: "Concluída"
+    }
+
+  ]
 
   return (
-    <>
+    <div>
+      {taskList.map((task) => {
+        console.log(<TaskForm titulo={task.title}/>)
+        console.log(<TaskForm desc={task.descricao}/>)
+        console.log(<TaskForm prioridade={task.priori}/>)
+        console.log(<TaskForm status={task.situacao}/>)
+          return <TaskForm titulo={task.title}/>
+        })}
       
-    </>
+    </div>
   )
 }
 
--- taskManager/src/components/TaskForm.jsx
+++ taskManager/src/components/TaskForm.jsx
@@ -1,18 +1,56 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 const TaskForm = () => {
   const [titulo, setTitulo] = useState("")
   const [desc, setDesc] = useState("")
   const [prioridade, setPrioridade] = useState("")
   const [status, setStatus] = useState("")
+  const [Titulos, setTitulos] = useState([])
+  const [Descs, setDescs] = useState([])
+  const [Prioridades, setPrioridades] = useState([])
+  const [situacoes, setSituacoes] = useState([])
+  function addTitulo(){
+    setTitulos(
+      [...Titulos, titulo]
+    )
+  }
+  useEffect(()=>{
+      console.log(titulo)
+    }, [Titulos])
   
+  function addDesc(){
+    setDescs(
+      [...Descs, desc]
+    )
+  }
+  useEffect(()=>{
+    console.log(desc)
+  }, [Descs])
+
+  function addPriori(){
+    setPrioridades(
+      [...Prioridades, prioridade]
+    )
+  }
+  useEffect(()=>{
+    console.log(prioridade)
+  }, [Prioridades])
+
+  function addStatus(){
+    setSituacoes(
+      [...situacoes, status]
+    )
+  }
+  useEffect(()=>{
+    console.log(status)
+  }, [situacoes])
   return (
     <>
-      <input type="name" value={titulo} onChange={e => setTitulo(e.target.value)}/>
-      <input type="text" value={desc} onChange={e => setDesc(e.target.value)}/>
-      <input type="text" value={prioridade} onChange={e => setPrioridade(e.target.value)}/>
-      <input type="text" value={status} onChange={e => setStatus(e.target.value)}/>
-      <button>Salvar Informações</button>
+      <input type="name" value={titulo} placeholder='Título da tarefa' onChange={e => setTitulo(e.target.value)}/>
+      <input type="text" value={desc} placeholder='Descrição da tarefa' onChange={e => setDesc(e.target.value)}/>
+      <input type="text" value={prioridade} placeholder='Prioridade' onChange={e => setPrioridade(e.target.value)}/>
+      <input type="text" value={status} placeholder='Status da tarefa' onChange={e => setStatus(e.target.value)}/>
+      <button onClick={() => {addTitulo(); addDesc(); addPriori(); addStatus()}}>Salvar Informações</button>
     </>
   )
 }
