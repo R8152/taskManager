7b457785a92d1e1575a82bfa774b48fff1bd53d7
R8152 <raulteles05@gmail.com>
2026-08-25 18:47:26 -0300

    feat: cria useEffect para salvar tarefas

--- taskManager/src/pages/Tasks.jsx
+++ taskManager/src/pages/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Tasks() {
   //Atualização automática de dados gravados anteriormente toda vez que a lista for alterada
@@ -7,6 +7,9 @@ export default function Tasks() {
       return savedTasks ? JSON.parse(savedTasks) : []
     })
     const [filtroStatus, setfiltroStatus] = useState("Todas") //Armazena a opção de filtragem selecionada pelo usuário na tela
+    useEffect(() => {
+      localStorage.setItem('tasks', JSON.stringify(tasks)) //useEffect para salvar as tarefas no localStorage sempre que a lista mudar
+    }, [tasks])
   return (
     <>
       
