cec59fbf885d0af7e7d7fd39be8a64567e52d7d1
R8152 <raulteles05@gmail.com>
2026-08-26 15:19:18 -0300

    fix: retira a função de salvar as tarefas

--- taskManager/src/pages/Tasks.jsx
+++ taskManager/src/pages/Tasks.jsx
@@ -7,9 +7,6 @@ export default function Tasks() {
   //Atualização automática de dados gravados anteriormente toda vez que a lista for alterada
     const [tasks, setTasks] = useState(tarefasIniciais)
     const [filtroStatus, setfiltroStatus] = useState("Todas") //Armazena a opção de filtragem selecionada pelo usuário na tela ou controla o filtro por status
-    useEffect(() => {
-      localStorage.setItem('tasks', JSON.stringify(tasks)) //useEffect para salvar as tarefas no localStorage sempre que a lista mudar
-    }, [tasks])
     const handleDelete = (id) => {
       setTasks(prevTasks => prevTasks.filter(task => task.id !== id))
     }
