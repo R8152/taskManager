6d60c5c4e4468a7b301d7ace935b8b44361255ec
R8152 <raulteles05@gmail.com>
2026-08-26 15:31:08 -0300

    feat: adiciona mudança funcional de página em uma nova tarefa

--- taskManager/src/pages/NewTask.jsx
+++ taskManager/src/pages/NewTask.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function NewTask() {
+  const navigate = useNavigate() //hook que permite mudar de página por meio de um componente funcional, como depois de enviar um formulário ou apertar um botão
+  
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
