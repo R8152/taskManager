c934600f98ee717f2894d9335a5016fad6f4cf38
R8152 <raulteles05@gmail.com>
2026-08-26 15:39:37 -0300

    feat: cria função a ser usada ao enviar o formulário

--- taskManager/src/pages/NewTask.jsx
+++ taskManager/src/pages/NewTask.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function NewTask() {
+export default function NewTask({setTasks}) {
   const navigate = useNavigate() //hook que permite mudar de página por meio de um componente funcional, como depois de enviar um formulário ou apertar um botão
-  
+  const handleAddTask = (novaTask) => {
+    if (setTasks){
+      setTasks(prevTasks => [...prevTasks, novaTask])
+    } else{
+      const savedTasks = JSON.parse(localStorage.getItem('tasks') || '[]')
+      const updatedTasks = [...savedTasks, novaTask]
+      localStorage.setItem('tasks', JSON.stringify(updatedTasks))
+    }
+
+    navigate('/tasks')
+  }
   return (
     <>
       
