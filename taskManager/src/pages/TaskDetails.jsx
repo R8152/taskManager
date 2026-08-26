ae7e911ad1092b2652d526c4b71ae9b5b70dd403
R8152 <raulteles05@gmail.com>
2026-08-26 16:19:52 -0300

    feat: cria mudança funcional de página em detalhes de uma tarefa

--- taskManager/src/pages/TaskDetails.jsx
+++ taskManager/src/pages/TaskDetails.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export default function TaskDetails() {
   return (
     const {id} = useParams() //hook que retorna um objeto com valores extraídos do endereço web
+    const navigate = useNavigate()
     <>
     
     </>
