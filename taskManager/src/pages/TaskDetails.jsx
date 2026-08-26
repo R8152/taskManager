dc4ab4df3f8f250c264e4ea69acfe13195886aed
R8152 <raulteles05@gmail.com>
2026-08-26 16:39:53 -0300

    feat: cria hook de retorno dos valores do endereço web

--- taskManager/src/pages/TaskDetails.jsx
+++ taskManager/src/pages/TaskDetails.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
-export default function TaskDetails() {
+export default function TaskDetails({tasks}) {
   return (
     const {id} = useParams() //hook que retorna um objeto com valores extraídos do endereço web
     const navigate = useNavigate()
+    const [task, setTask] = useState(null)
+    useEffect(() => {
+      //Buscando das props 'tasks' ou direto de localStorage
+      let currentTasks = tasks
+      if(!currentTasks || currentTasks.length === 0){
+        const savedTasks = localStorage.getItem('tasks')
+        currentTasks = savedTasks ? JSON.parse(savedTasks) : []
+      }
+
+      const foundTask = currentTasks.find(item => String(item.id) === String(id))
+
+      if (foundTask){
+        setTask(foundTask)
+      }
+    }, [id, tasks])
     <>
     
     </>
