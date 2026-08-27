02ebb686ad9461b20f78a48f5e413560d88aa01b
R8152 <raulteles05@gmail.com>
2026-08-27 10:00:59 -0300

    feat: adiciona condição para não encontrar uma tarefa

--- taskManager/src/pages/TaskDetails.jsx
+++ taskManager/src/pages/TaskDetails.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 
 export default function TaskDetails({tasks}) {
-  return (
-    const {id} = useParams() //hook que retorna um objeto com valores extraídos do endereço web
+  const {id} = useParams() //hook que retorna um objeto com valores extraídos do endereço web
     const navigate = useNavigate()
     const [task, setTask] = useState(null)
     useEffect(() => {
@@ -20,6 +19,16 @@ export default function TaskDetails({tasks}) {
         setTask(foundTask)
       }
     }, [id, tasks])
+    if (!task){
+      return(
+        <div>
+          <h2>Tarefa não encontrada</h2>
+          <p>A tarefa não existe ou foi removida</p>
+          <button type="button" onClick={() => navigate('/tasks')}>Voltar para a lista</button>
+        </div>
+      )
+    }
+  return (
     <>
     
     </>
