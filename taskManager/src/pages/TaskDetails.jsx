4f2da08aedaa258da19922be57214bf5cda11868
R8152 <raulteles05@gmail.com>
2026-08-27 10:28:04 -0300

    fix: conserta a forma de buscar dados nos detalhes de uma tarefa

--- taskManager/src/pages/TaskDetails.jsx
+++ taskManager/src/pages/TaskDetails.jsx
@@ -1,31 +1,23 @@
-import { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 export default function TaskDetails({tasks}) {
   const {id} = useParams() //hook que retorna um objeto com valores extraídos do endereço web
-    const navigate = useNavigate()
-    const [task, setTask] = useState(null)
-    useEffect(() => {
-      //Buscando das props 'tasks' ou direto de localStorage
-      let currentTasks = tasks
-      if(!currentTasks || currentTasks.length === 0){
-        const savedTasks = localStorage.getItem('tasks')
-        currentTasks = savedTasks ? JSON.parse(savedTasks) : []
-      }
-
-      const foundTask = currentTasks.find(item => String(item.id) === String(id))
-
-      if (foundTask){
-        setTask(foundTask)
-      }
-    }, [id, tasks])
-    if (!task){
-      return(
-        <div>
-          <h2>Tarefa não encontrada</h2>
-          <p>A tarefa não existe ou foi removida</p>
-          <button type="button" onClick={() => navigate('/tasks')}>Voltar para a lista</button>
-        </div>
+  const navigate = useNavigate()
+  //Buscando das props 'tasks' ou direto de localStorage
+  //Não se deve utilizar useState ou useEffect para calcular, derivar ou buscar um dado por meio de props ou estados existentes
+  let currentTasks = tasks
+  if(!currentTasks || currentTasks.length === 0){
+    const savedTasks = localStorage.getItem('tasks')
+    currentTasks = savedTasks ? JSON.parse(savedTasks) : []
+  }
+  const task = currentTasks.find(item => String(item.id) === String(id))
+  if (!task){
+    return(
+      <div>
+        <h2>Tarefa não encontrada</h2>
+        <p>A tarefa não existe ou foi removida</p>
+        <button type="button" onClick={() => navigate('/tasks')}>Voltar para a lista</button>
+      </div>
       )
     }
   return (
