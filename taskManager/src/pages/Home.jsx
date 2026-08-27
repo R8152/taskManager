cc4956269f8e1bd6c90f8d45bef6571350bbc038
R8152 <raulteles05@gmail.com>
2026-08-27 10:37:42 -0300

    feat: cria página de entrada

--- taskManager/src/pages/Home.jsx
+++ taskManager/src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Task Manager</h1>
+      <p>
+        Aplicação simples para gerenciamento e controle de tarefas internas.
+        Cadastre, filtre e acompanhe o progresso das suas atividades.
+      </p>
+      <Link to="/tasks">
+        <button type="button">Acessar lista de tarefas</button>
+      </Link>
+    </>
   )
 }
